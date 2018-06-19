9f91b8b4227da4fd60dd3bde783dbea746339dd9
Daniel Walz <walzdan@gmail.com>
2018-06-19 11:20:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var cart = [];
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
 function getCart() {
  return cart;
 }
@@ -9,11 +13,28 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
+function addToCart(name) {
  // write your code here
+ let item = {
+   itemName: name,
+   itemPrice: getRandomInt(100)
+ };
+ cart.push(item);
+ return `${item.itemName} has been added to your cart.`;
 }
 
-function viewCart() {
+function viewCart() { 
+  let message = "Your shopping cart is empty.";
+ cart.forEach(function (item, index) {
+   if (index === 0) {
+     message = `In your cart, you have ${item.itemName} at ${item.itemPrice}, `;
+   } else if (index > 0 && index !== -1) { 
+   message + `${item.itemName} at ${item.itemPrice}, `;
+   } else { 
+     message + `and ${item.itemName} at ${item.itemPrice}.`;
+   }
+  return message
+});
   // write your code here
 }
 
